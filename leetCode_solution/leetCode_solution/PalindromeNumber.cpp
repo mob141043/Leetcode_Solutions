dd840a1285c7e994e5883c78e0e2a1d9a82e1954
mob141043 <mob5832@gmail.com>
2021-04-21 23:42:31 +0700

    21-04-2021

---
+++ leetCode_solution/leetCode_solution/PalindromeNumber.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include<string>
+using namespace std;
+
+bool isPalindrome(int x) {
+	if (x < 0) {
+		return false;
+	}
+	string s = to_string(x);
+	int size = s.size();
+	for (int i = 0, j = size - 1; j >= i; i++, j--) {
+		if (s.at(i) != s.at(j)) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	
+}
+
